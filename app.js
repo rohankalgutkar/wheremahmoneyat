4f74b18f92c6823ca4fcec83c9c9c493ed0f2ebb
rohankalgutkar@gmail.com <rohankalgutkar@gmail.com>
2019-04-16 23:45:14 +0530

    v2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const hbs = require('express-hbs')
-const port = 3000
+const port = process.env.PORT || 3000;
 var data = require('./dummy-data.json')
 
 var app = express()
